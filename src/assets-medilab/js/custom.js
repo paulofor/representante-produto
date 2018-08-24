12d05ea85281c43b74b164c61d87a323af7e2c2b
Paulo Alexandre Lopes Forestieri <tr626987@oi.corp.net>
2018-08-24 14:51:53 -0300

    testando menu

--- src/assets-medilab/js/custom.js
+++ src/assets-medilab/js/custom.js
@@ -1,42 +1,45 @@
-(function($) {
 
-  // Add smooth scrolling to all links in navbar
-  $(".navbar a,a.btn-appoint, .quick-info li a, .overlay-detail a").on('click', function(event) {
 
-    var hash = this.hash;
-    if (hash) {
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 900, function() {
-        window.location.hash = hash;
-      });
-    }
+  $(document).ready(function(){
 
-  });
-
-  $(".navbar-collapse a").on('click', function() {
-    $(".navbar-collapse.collapse").removeClass('in');
-  });
-
-  $(function () {
     $("#myNavbar a").click(function () {
-      alert('vai viajar');
+      alert('vai viajar 2');
       var href = $(this).attr("href");
       $('html, body').animate({
         scrollTop: $(href).offset().top
       }, 500);
       return false;
     });
+
+    $(".navbar-collapse a").on('click', function() {
+      $(".navbar-collapse.collapse").removeClass('in');
+    });
+
+
+    $(".navbar a,a.btn-appoint, .quick-info li a, .overlay-detail a").on('click', function(event) {
+
+      var hash = this.hash;
+      if (hash) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 900, function() {
+          window.location.hash = hash;
+        });
+      }
+  
+    });
+
+    $(window).scroll(function() {
+      if ($(".navbar-default").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+      } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+      }
+    });
+
   });
 
-  //jQuery to collapse the navbar on scroll
-  $(window).scroll(function() {
-    if ($(".navbar-default").offset().top > 50) {
-      $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-      $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-  });
 
-})(jQuery);
+
+
